df39d7f5a901f80c2ebdf8c942c707caff9c20f8
Luis <64982212+Ltotheois@users.noreply.github.com>
2021-11-06 22:19:52 +0100

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# # F*ck
+This repository provides a digital version for the scoring of the card game
+
---
+++ sw.js
@@ -0,0 +1,36 @@
+// version 1.00
+// change version to reinstall service worker and update Files
+
+var cacheName = 'cache_fck';
+var filesToCache = [
+	'/Fck/',
+	'/Fck/fontawesome/css/all.css',
+	'/Fck/favicon.svg',
+	'/Fck/stylesheet.css',
+	'/Fck/Fck.html',
+	'/Fck/manifest.json',
+	'/Fck/fontawesome/webfonts/fa-regular-400.woff2',
+	'/Fck/roboto/roboto-v20-latin-regular.woff2',
+	'/Fck/fontawesome/webfonts/fa-solid-900.woff2',
+	'/Fck/title.png'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+  caches.delete(cacheName);
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
